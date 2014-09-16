8de9e0b3d3a937f44cf44880cf32281e481809d9
Gregory Weaver <gregory.weaver@gmail.com>
2014-09-16 13:32:29 -0700

    Initial commit.

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'loganov-nginx'
+maintainer       'Loganov Industries, LLC'
+maintainer_email 'devops@loganov.com'
+license          'All rights reserved'
+description      'Installs/Configures loganov-nginx'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends          'nginx'
---
+++ recipes/default.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook Name:: loganov-nginx
+# Recipe:: default
+#
+# Copyright 2014, Loganov Industries LLC
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe "nginx::default"
